53ac98468c7540256e70d77882205f3c17d90cfc
jayanth <jayanthk468@gmail.com>
2025-08-03 06:01:38 +0530

    first commit

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.48.0"
+    }
+}
+backend "s3"{
+        bucket = "jayanth-remote-state1"
+        key = "vpc-module-test1"
+        region = "us-east-1"
+        dynamodb_table = "daws78s-locking"
+
+    }
+}
+
+    provider "aws" {
+    region = "us-east-1"
+
+}
